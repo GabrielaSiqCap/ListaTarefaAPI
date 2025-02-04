2b7fc9e51735be931b1f040732ac91db61c5b07d
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-02-04 16:56:22 -0300

    Inclusão do Banco de dados

---
+++ ListaTarefaAPI/Data/ListaTarefasContext.cs
@@ -0,0 +1,16 @@
+﻿using ListaTarefaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefaAPI.Data
+{
+    public class ListaTarefasContext : DbContext
+    {
+        public ListaTarefasContext
+            (DbContextOptions<ListaTarefasContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<TipoTarefa> TiposTarefas { get; set; }
+    }
+}
